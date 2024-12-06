c8d984b5c9afd8688ff6fa919c12c5285670cf35
Pinguladora <alvaroechegoyan2000@gmail.com>
2024-12-06 17:01:10 +0100

    feat: add day 6 challenge

---
+++ 6/index.js
@@ -0,0 +1,15 @@
+/** 
+ * @param {string[]} gifts
+ * @returns {boolean} True if the gift is inside the box
+ */
+function inBox(box) {
+    // Discard first and last elements as they are the bottom and top edges which are not allowed
+    for (let i = 1; i < box.length - 1; i++) {
+        const asteriskIndex = box[i].indexOf("*")
+        // Search for the asterik between the lateral edges
+        if (asteriskIndex > 0 && asteriskIndex < box[i].length - 1) {
+            return true
+        }
+    }
+    return false
+}
