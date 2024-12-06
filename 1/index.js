28e400a8d0b000d5c97ca4a67d298d0540a34e17
Pinguladora <alvaroechegoyan2000@gmail.com>
2024-12-06 16:58:35 +0100

    docs: add JSDoc annotations

--- 1/index.js
+++ 1/index.js
@@ -1,3 +1,7 @@
+/**
+ * @param {number[]} gifts - The array of gifts to prepare
+ * @returns {number[]} An array with the prepared gifts
+ */
 function prepareGifts(gifts) {
   // Use a Set to remove duplicates directly, then sort numerically
   return [...new Set(gifts)].sort((a,b) => a-b);
