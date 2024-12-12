9ff6cbcdc5aa12051b461110b5cba2cd995fe041
Pinguladora <alvaroechegoyan2000@gmail.com>
2024-12-12 16:03:32 +0100

    feat: add day 10 in TypeScript

---
+++ 10/index.ts
@@ -0,0 +1,25 @@
+function compileTS(instructions: string[]): number | undefined {
+    const registers: { [key: string]: number } = {};
+    let i = 0;
+
+    while (i < instructions.length) {
+        const [cmd, ...args] = instructions[i].split(" ");
+
+        if (cmd === "MOV") {
+            const [val, reg] = args;
+            registers[reg] = Number(val) ? Number(val) : (registers[val] || 0);
+        } else if (cmd === "INC") {
+            registers[args[0]] = (registers[args[0]] || 0) + 1;
+        } else if (cmd === "DEC") {
+            registers[args[0]] = (registers[args[0]] || 0) - 1;
+        } else if (cmd === "JMP") {
+            const [reg, idx] = args;
+            if ((registers[reg] || 0) === 0) {
+                i = parseInt(idx);
+                continue;
+            }
+        }
+        i++;
+    }
+    return registers["A"];
+}
