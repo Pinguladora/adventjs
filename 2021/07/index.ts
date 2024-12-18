4c4899e5a8e5bfdf974412535093c7f5405fc70f
Pinguladora <alvaroechegoyan2000@gmail.com>
2024-12-18 21:37:43 +0100

    feat: add day 7 of 2021 using TypeScript

---
+++ 2021/07/index.ts
@@ -0,0 +1,68 @@
+// Recursive
+export default function contains(store: object | string, product: string): boolean {
+    // Base case
+    if (typeof store !== 'object' || store === null) {
+        return store === product
+    }
+
+    for (let key in store) {
+        if (contains(store[key], product)) {
+            return true
+        }
+    }
+    return false
+}
+
+// Iterative
+export function containsIterative(store: object | string, product: string): boolean {
+    // Use a stack to hold the store
+    const stack = [store];
+
+    while (stack.length > 0) {
+        const item = stack.pop();
+
+        if (item === product) {
+            return true;
+        }
+
+        // If the current value is an object, add its values to the stack
+        if (typeof item === 'object' && item !== null) {
+            for (let key in item) {
+                stack.push(item[key]);
+            }
+        }
+    }
+    return false
+}
+
+
+const almacen = {
+    'estanteria1': {
+        'cajon1': {
+            'producto1': 'coca-cola',
+            'producto2': 'fanta',
+            'producto3': 'sprite'
+        }
+    },
+    'estanteria2': {
+        'cajon1': 'vacio',
+        'cajon2': {
+            'producto1': 'pantalones',
+            'producto2': 'camiseta' // <- ¡Está aquí!
+        }
+    }
+}
+
+containsIterative(almacen, 'camiseta') // true
+
+const otroAlmacen = {
+    'baul': {
+        'fondo': {
+            'objeto': 'cd-rom',
+            'otro-objeto': 'disquette',
+            'otra-cosa': 'mando'
+        }
+    }
+}
+
+containsIterative(otroAlmacen, 'gameboy') // false
