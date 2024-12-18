10def2e83367c415f894f122aa24f1d3b86edd34
Pinguladora <alvaroechegoyan2000@gmail.com>
2024-12-18 21:36:18 +0100

    feat: add day 6 challenge 2021

---
+++ 2021/06/index.ts
@@ -0,0 +1,17 @@
+// Complexity: O(n^2) 
+export default function sumPairs(numbers: number[], result: number): number[] | null {
+    for (let i = 0; i < numbers.length; i++) {
+        const num = numbers[i]
+        const toSearch = result - num
+        if (numbers.includes(toSearch, i + 1)) {
+            return [num, toSearch]
+        }
+    }
+    return null
+}
+
+sumPairs([3, 5, 7, 2], 10) // [3, 7]
+sumPairs([-3, -2, 7, -5], 10) // null
+sumPairs([2, 2, 3, 1], 4) // [2, 2]
+sumPairs([6, 7, 1, 2], 8) // [6, 2]
+sumPairs([0, 2, 2, 3, -1, 1, 5], 6) // [1, 5]
