c2c317e0ea7a79edcc1d1f2bbe852b48a0b4f57e
Pinguladora <alvaroechegoyan2000@gmail.com>
2024-12-19 16:17:52 +0100

    feat: add day 9 challenge 2021

---
+++ 2021/09/index.js
@@ -0,0 +1,27 @@
+export default function groupBy(collection, it) {
+    return collection.reduce((accum, item) => {
+        const groupKey = item[it] || it(item)
+        accum[groupKey] = accum[groupKey] || []
+        accum[groupKey].push(item)
+        return accum
+    }, {})
+}
+
+groupBy([6.1, 4.2, 6.3], Math.floor) // { 6: [6.1, 6.3], 4: [4.2] }
+groupBy(['one', 'two', 'three'], 'length') // { 3: ['one', 'two'], 5: ['three'] }
+groupBy([{ age: 23 }, { age: 24 }], 'age') // { 23: [{age: 23}], 24: [{age: 24}] }
+
+groupBy(
+    [1397639141184, 1363223700000],
+    timestamp => new Date(timestamp).getFullYear()
+)
+// { 2013: [1363223700000], 2014: [1397639141184] }
+
+groupBy([
+    { title: 'JavaScript: The Good Parts', rating: 8 },
+    { title: 'Aprendiendo Git', rating: 10 },
+    { title: 'Clean Code', rating: 9 },
+], 'rating')
+// { 8: [{ title: 'JavaScript: The Good Parts', rating: 8 }],
+//   9: [{ title: 'Clean Code', rating: 9 }],
+//   10: [{ title: 'Aprendiendo Git', rating: 10 }] }
