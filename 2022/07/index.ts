6d5f2c17d1ecffa747ea8eeb6d2bbddee28008c4
Pinguladora <alvaroechegoyan2000@gmail.com>
2024-12-21 21:55:26 +0100

    feat: add day 7 of 2022 in TypeScript

---
+++ 2022/07/index.ts
@@ -0,0 +1,20 @@
+export default function getGiftsToRefill(a1: string[], a2: string[], a3: string[]): string[] {
+    const merged = [...new Set([...a1, ...a2, ...a3])];
+    return merged.filter(g =>
+        [a1.includes(g), a2.includes(g), a3.includes(g)]
+            .filter(Boolean).length === 1
+    );
+}
+
+const a1 = ['bike', 'car', 'bike', 'bike']
+const a2 = ['car', 'bike', 'doll', 'car']
+const a3 = ['bike', 'pc', 'pc']
+
+/* The store a1 has "bike" and "car".
+The store a2 has "car", "bike" and "doll".
+The store a3 has "bike" and "pc".
+ 
+The gift "doll" and "pc" are only in the stores a2 and a3 respectively.
+*/
+
+const gifts = getGiftsToRefill(a1, a2, a3) // ['doll', 'pc']
