ae925c44b0ac55c940d4e527929d3453314b313c
Pinguladora <alvaroechegoyan2000@gmail.com>
2024-12-21 21:56:03 +0100

    feat: add day 8 of 2022 in TypeScript

---
+++ 2022/08/index.ts
@@ -0,0 +1,27 @@
+export default function checkPart(part: string): boolean {
+    let left = 0;
+    let right = part.length - 1;
+
+    while (left < right) {
+        if (part[left] !== part[right]) {
+            return part.slice(left + 1, right + 1) ===
+                [...part.slice(left + 1, right + 1)].reverse().join("") ||
+                part.slice(left, right) ===
+                [...part.slice(left, right)].reverse().join("");
+        }
+        left++;
+        right--;
+    }
+
+    return true;
+}
+
+
+checkPart("uwu") // true
+// "uwu" is a palindrome without removing any character
+
+checkPart("miidim") // true
+// "miidim" can be a palindrome after removing the first "i"
+
+checkPart("midu") // false
+// "midu" cannot be a palindrome after removing a character
