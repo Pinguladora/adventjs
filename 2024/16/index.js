ffe23d5b514057d132372dffeb3a711fdd488654
Pinguladora <alvaroechegoyan2000@gmail.com>
2024-12-24 00:11:58 +0100

    feat: add day 16 challenge 2024

---
+++ 2024/16/index.js
@@ -0,0 +1,29 @@
+/**
+ * @param {string} s
+ * @returns {string}
+ */
+function removeSnow(s) {
+    const clearPath = [];
+    for (const char of s) {
+        const pathSize = clearPath.length;
+        if (pathSize > 0 && clearPath[pathSize - 1] === char) {
+            clearPath.pop();
+            continue;
+        }
+        clearPath.push(char);
+    }
+    return clearPath.join("");
+}
+
+removeSnow('zxxzoz') // -> "oz"
+// 1. Remove "xx", resulting in "zzoz"
+// 2. Remove "zz", resulting in "oz"
+
+removeSnow('abcdd') // -> "abc"
+// 1. Remove "dd", resulting in "abc"
+
+removeSnow('zzz') // -> "z"
+// 1. Remove "zz", resulting in "z"
+
+removeSnow('a') // -> "a"
+// No duplicate piles
