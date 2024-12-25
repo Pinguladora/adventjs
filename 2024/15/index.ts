08ebdc8e1b32c6464293b889e0e2597d22404dac
Pinguladora <alvaroechegoyan2000@gmail.com>
2024-12-25 14:32:42 +0100

    feat: add day 15 of 2024 in TypeScript

--- 2024/15/index.ts
+++ 2024/15/index.ts
@@ -1,6 +1,50 @@
-function minMovesToStablesTS(reindeer: number[], stables: number[]): number {
-    const sortedReindeer = [...reindeer].sort((a, b) => a - b);
-    const sortedStables = [...stables].sort((a, b) => a - b);
+export default function drawTable(data: Array<Record<string, string | number>>): string {
+    const columns = Object.keys(data[0]);
+    const headers: string[] = [];
+    for (let col of columns) {
+        headers.push(col.charAt(0).toUpperCase() + col.slice(1));
+    }
 
-    return sortedReindeer.reduce((total, r, i) => total + Math.abs(r - sortedStables[i]), 0);
+    // Calculate column widths
+    const colWidths = new Array(columns.length).fill(0);
+    for (let i = 0; i < columns.length; i++) {
+        colWidths[i] = headers[i].length;
+
+        // Search for maximum width
+        for (let j = 0; j < data.length; j++) {
+            const cellWidth = String(data[j][columns[i]]).length;
+            if (cellWidth > colWidths[i]) {
+                colWidths[i] = cellWidth;
+            }
+        }
+    }
+
+    let border = "+";
+    for (let width of colWidths) {
+        border += "-".repeat(width + 2) + "+";
+    }
+
+    function createRow(values: string[]) {
+        let row = "|";
+        for (let i = 0; i < values.length; i++) {
+            const paddedValue = String(values[i]).padEnd(colWidths[i]);
+            row += ` ${paddedValue} |`;
+        }
+        return row;
+    }
+
+    const headerRow = createRow(headers);
+
+    const dataRows: any[] = [];
+    for (let i = 0; i < data.length; i++) {
+        const rowValues: any[] = [];
+        for (let j = 0; j < columns.length; j++) {
+            const val = data[i][columns[j]]
+            rowValues.push(val);
+        }
+        dataRows.push(createRow(rowValues));
+    }
+
+    const table = [border, headerRow, border, ...dataRows, border].join("\n");
+    return table;
 }
