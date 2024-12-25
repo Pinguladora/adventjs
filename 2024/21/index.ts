9fea707c6c231999286f605c3e9aa48736ed20c7
Pinguladora <alvaroechegoyan2000@gmail.com>
2024-12-25 12:17:55 +0100

    feat: add day 21 of 2024 in TypeScript

---
+++ 2024/21/index.ts
@@ -0,0 +1,52 @@
+/**
+   * @param {{ value: string; left: any; right: any }} tree
+   * @returns {number} - Height of the tree.
+   */
+export default function treeHeight(tree: { value: string; left: any; right: any } | null): number {
+    // Base case
+    if (tree === null) {
+        return 0;
+    }
+    const leftHeight = treeHeight(tree.left);
+    const rightHeight = treeHeight(tree.right);
+
+    return Math.max(leftHeight, rightHeight) + 1;
+}
+
+// Tree definition
+const tree = {
+    value: '🎁',
+    left: {
+        value: '🎄',
+        left: {
+            value: '⭐',
+            left: null,
+            right: null
+        },
+        right: {
+            value: '🎅',
+            left: null,
+            right: null
+        }
+    },
+    right: {
+        value: '❄️',
+        left: null,
+        right: {
+            value: '🦌',
+            left: null,
+            right: null
+        }
+    }
+}
+
+// Graphical representation of the tree:
+//        🎁
+//       /   \
+//     🎄     ❄️
+//    /  \      \
+//  ⭐   🎅      🦌
+
+// Function call
+treeHeight(tree)
+// Returns: 3
