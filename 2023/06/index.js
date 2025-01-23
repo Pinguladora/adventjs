f6e77c9004918b0dffb3d4e6b27613896eb46b03
Pinguladora <alvaroechegoyan2000@gmail.com>
2025-01-23 01:42:43 +0100

    feat: add day 6 of 2023

---
+++ 2023/06/index.js
@@ -0,0 +1,25 @@
+function maxDistance(movements) {
+    let leftCount = 0;
+    let rightCount = 0;
+    let starCount = 0;
+
+    for (const mov of movements) {
+        if (mov === '<') leftCount++;
+        else if (mov === '>') rightCount++;
+        else starCount++;
+    }
+
+    return Math.abs(rightCount - leftCount) + starCount;
+}
+
+const movements = '>>*<'
+const result = maxDistance(movements)
+console.log(result) // -> 2
+
+const movements2 = '<<<>'
+const result2 = maxDistance(movements2)
+console.log(result2) // -> 2
+
+const movements3 = '>***>'
+const result3 = maxDistance(movements3)
+console.log(result3) // -> 5
